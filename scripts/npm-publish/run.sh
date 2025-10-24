d5dc830c0dbff0014540660d488a997fb6a825eb
jonasnobile <mesons_rates0p@icloud.com>
2025-10-24 16:41:45 +0200

    chore: update npm publish

--- scripts/npm-publish/run.sh
+++ scripts/npm-publish/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
-
 for dir in packages/*; do
   if [ -f "$dir/package.json" ]; then
     if ! grep -q '"private": true' "$dir/package.json"; then
