d5e1ac8ec6c3e479b688413abcc0f27771baff88
jonasnobile <mesons_rates0p@icloud.com>
2025-10-25 21:35:20 +0200

    chore: generate provenance for packages

--- scripts/npm-publish/run.sh
+++ scripts/npm-publish/run.sh
@@ -4,7 +4,7 @@ set -e
 for dir in packages/*; do
   if [ -f "$dir/package.json" ]; then
     if ! grep -q '"private": true' "$dir/package.json"; then
-      (cd "$dir" && npm publish --tag "$NPM_TAG" --access public)
+      (cd "$dir" && npm publish --tag "$NPM_TAG" --provenance --access public)
     fi
   fi
 done
