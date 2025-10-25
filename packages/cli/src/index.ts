0aa759834c8486bdee3d5495e4fdc72444f8d945
jonasnobile <mesons_rates0p@icloud.com>
2025-10-26 01:03:34 +0200

    refactor(cli): move build command to second argument

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,6 +2,28 @@
 import { agentsSummary } from '@nuasite/agent-summary'
 import { build } from './build.js'
 
-await build({
-	integrations: [agentsSummary()],
-})
+const command = process.argv[2]
+
+function printUsage() {
+	console.log('Usage: nua <command>')
+	console.log('\nCommands:')
+	console.log('  build   Run astro build with the Nua defaults')
+	console.log('  help    Show this message\n')
+}
+
+switch (command) {
+	case 'build':
+		await build({
+			integrations: [agentsSummary()],
+		})
+		break
+	case 'help':
+	case '--help':
+	case '-h':
+		printUsage()
+		break
+	default:
+		console.error(command ? `Unknown command: ${command}` : 'No command specified.')
+		printUsage()
+		process.exit(1)
+}
